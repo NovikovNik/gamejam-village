a3e612a07998b518b02ef63e3df6958b415f2f7b
Николай Новиков <nikolay.novikov@playrix.com>
2026-02-22 12:12:54 +0100

    Location fixes

--- src/Game/GameFeatures.h
+++ src/Game/GameFeatures.h
@@ -6,7 +6,7 @@ enum class MovementDirection {
 };
 
 struct GameFeatures {
-    inline static std::string gameTitle = "Test Game";
+    inline static std::string gameTitle = "AAAB";
     inline static bool isDebug = false;
     inline static bool isFullscreen = false;
     inline static bool isResizeble = false;
